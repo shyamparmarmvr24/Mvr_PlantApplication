74a084f06ca95e1eb89385f2b629ae0a182474d2
srikanthmvrdeveloper-dot <srikanthmvr.developer@gmail.com>
2025-11-29 10:13:53 +0530

    Update IPlantService.java

--- MVR-Plant-Project/src/main/java/com/mvr/plant/service/IPlantService.java
+++ MVR-Plant-Project/src/main/java/com/mvr/plant/service/IPlantService.java
@@ -11,4 +11,5 @@ public interface IPlantService
     public FstpPlant getPlantById(Long id);
     public String updatePlantDetails(Long id,FstpPlant plant);
     public List<FstpPlant> getPlantsByZone(Integer zone);
+    // no change done test file
 }
